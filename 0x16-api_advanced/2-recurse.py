177eca3bbd92e724368e7ec99339e49aee5dc0ac
somarae8 <somarae8@gmail.com>
2020-09-17 22:09:33 -0500

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+titles of the first 10 hot posts listed
+"""
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    returns a list containing the titles of 
+    all hot articles for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'User-Agent': 'somarae8'}
+    info = requests.get(url, headers=header, allow_redirects=False, params={'after': after})
+    if info.status_code != 200:
+        return None
+    else:
+        after = info.json()['data']['after']
+        if after is None:
+            return hot_list
+        for post in info.json()['data']['children']:
+            hot_list.append(post['data']['title'])
+        return recurse(subreddit, hot_list, after)
